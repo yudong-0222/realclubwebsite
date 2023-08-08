1a4ccef84fc28863191d9a2d1d757cc823262ee5
RedFeaTherXll <yuchi9716@gmail.com>
2023-08-08 19:45:38 +0800

    將Nabbar的連結翻譯成中文 我怕農校的人看不懂(jk

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
     >
       {/* 未來加上Logo */}
       {/* <img className=' max-w-[64px] w-[64px] sm:w-[64px] md:w-[64px] lg:w-[64px] rounded-xl' src='/images/super2.png' alt="/" /> */}
-      <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white mt-2 font-extrabold'>
+      <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white mt-2 font-bold'>
         <a href="/">
           <div className='mt-6 mr-10'>
             <h1 className='min-w-[81px] w-full text-2xl text-[#fa7cd7]'>ASHS.NUTN</h1>
@@ -29,35 +29,35 @@ const Navbar = () => {
         </a>
         <ul className='hidden : md:flex'>
           <a href="/">
-            <li className='p-4 rounded-full hover:border-y-2 border-[#25AAE1]'>HOME</li>
+            <li className='p-4 rounded-full hover:border-y-2 border-[#25AAE1]'>主頁</li>
           </a>
           <a href="/link">
-            <li className='p-4 rounded-full hover:border-y-2 border-[#25AAE1]'>LINK</li>
+            <li className='p-4 rounded-full hover:border-y-2 border-[#25AAE1]'>連結</li>
           </a>
           <a href="/founder">
-            <li className='p-4 rounded-full hover:border-y-2 border-[#25AAE1]'>FOUNDER</li>
+            <li className='p-4 rounded-full hover:border-y-2 border-[#25AAE1]'>創辦團隊</li>
           </a>
           <a href="/contact">
-              <li className='p-4 rounded-full hover:border-y-2 border-[#25AAE1]'>CONTACT</li>
+              <li className='p-4 rounded-full hover:border-y-2 border-[#25AAE1]'>與我們聯繫</li>
           </a>
         </ul>
         <div onClick={handleNav} className='block md:hidden'>
           {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/> }
         </div>
         <div className={!nav ? 'z-10 flex flex-col absolute left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500  md:hidden' : 'fixed left-[-100%] top-0 ease-in duration-500 block'}>
-          <h1 className='w-full text-3xl font-bold text-[#fc87ac] m-4 '>所有頁面</h1>
+          <h2 className='text-3xl font-bold text-[#fa7cd7] m-4 '>南大附中<br/><div className='text-[#25AAE1]'>資訊社</div></h2> 
           <ul className='uppercase p-4'>
             <a href="/">
-              <li className='p-4 border-b border-gray-600'>HOME</li>
+              <li className='p-4 border-b border-gray-600'>主頁</li>
             </a>
             <a href="/link">
-              <li className='p-4 border-b border-gray-600'>Link</li>
+              <li className='p-4 border-b border-gray-600'>連結</li>
             </a>
             <a href="/founder">
-              <li className='p-4 border-b border-gray-600'>Founder</li>
+              <li className='p-4 border-b border-gray-600'>創辦團隊</li>
             </a>
             <a href="/contact">
-              <li className='p-4 '>Contact</li>
+              <li className='p-4 '>與我們聯繫</li>
             </a>
           </ul>
         </div>
