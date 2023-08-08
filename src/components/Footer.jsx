ae2d3c613d06754583af92c6073ca2c15c5cee57
RedFeaTherXll <yuchi9716@gmail.com>
2023-08-08 19:47:52 +0800

    Remake the Footer composition

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,20 +13,20 @@ const Footer = () => {
     initial="hidden"
     whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
+    viewport={{ once: true, amount: 0.3}}
   >
     {/* <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}> */}
     {/* </div> */}
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
       <div className="flex flex-col">
-        <div className="mb-[50px] h-[2px] bg-white opacity-10" />
-        <div className="flex items-center justify-between flex-wrap gap-4">
-          <h4 className="w-full text-xl font-bold text-[#fa7cd7]">ASHS.NUTN</h4>
-          <h4 className="w-20px text-3xl font-bold text-[#52a5e5]">CSIRC</h4>
-          <p className="font-normal text-[14px] text-white opacity-50">
-            ASHS.NUTN CSIRC Since 2023
+        <div className="mb-[50px] h-[2px] bg-white opacity-10 " />
+        <h4 className="w-full text-xl font-bold text-[#fa7cd7] text-center">ASHS.NUTN</h4>
+        <h4 className="w-full text-xl font-bold text-[#7cc8fa] text-center">CSIRC</h4>
+        <div className="flex items-center justify-center flex-wrap gap-4">
+          <p className="w-full font-normal text-[14px] text-white opacity-50 text-center items-center">
+            Since 2023
           </p>
-
-          <div className="flex gap-4 ">
+          <div className="flex gap-4">
             <a href='https://www.instagram.com/ashs.nutn_csirc/' target='_blank' rel='noopener noreferrer'>                
               <FaInstagram className="w-[24px] h-[24px] object-contain cursor-pointer rounded-md bg-[#DA2E69]"/>
             </a>
